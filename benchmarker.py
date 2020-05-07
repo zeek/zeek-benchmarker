b2be8039065f6d913d990b14e4e9cf83b546d5a6
Tim Wojtulewicz <tim@corelight.com>
2020-05-07 12:37:24 -0400

    Add support for running builds from local directories

--- benchmarker.py
+++ benchmarker.py
@@ -14,6 +14,7 @@ from flask import Flask, request
 # *must* be prefixed with 'b' or generating the digest to test with won't work.
 HMAC_KEY = b''
 DATA_FILE = ''
+HOST_IP=''
 
 app = Flask(__name__)
 app.config['CPU_SET'] = [22, 23]
@@ -35,17 +36,6 @@ def verify_hmac(request_path, timestamp, request_digest):
 @app.route('/zeek', methods=['POST'])
 def zeek():
 
-        hmac_header = request.headers.get('Zeek-HMAC', None)
-        if not hmac_header:
-                return 'HMAC header missing from request', 403
-
-        hmac_timestamp = int(request.headers.get('Zeek-HMAC-Timestamp', 0))
-        if not hmac_timestamp:
-                return 'HMAC timestamp missing from request', 403
-
-        if not verify_hmac(request.path, hmac_timestamp, hmac_header):
-                return 'HMAC validation failed', 403
-
         branch = request.args.get('branch', '')
         if not branch:
                 return 'Branch argument required', 400
@@ -54,14 +44,31 @@ def zeek():
         if not build_url:
                 return 'Build argument required', 400
 
-        # Validate that the build URL is coming from Cirrus. This shouldn't be necessary but it's
-        # easy sanity check.
-        if not build_url.startswith('https://api.cirrus-ci.com/v1/artifact/build'):
-                return 'Invalid build URL', 400
+        # Validate that the build URL is either from Cirrus, or a local file from the local host.
+        if build_url.startswith('https://api.cirrus-ci.com/v1/artifact/build'):
+                remote_build = True
 
-        build_hash = request.args.get('build_hash', '')
-        if not build_hash:
-                return 'Build hash argument required', 400
+                # Remote requests are required to be signed with HMAC and have an md5 hash
+                # of the build file passed with them.
+                hmac_header = request.headers.get('Zeek-HMAC', None)
+                if not hmac_header:
+                        return 'HMAC header missing from request', 403
+
+                hmac_timestamp = int(request.headers.get('Zeek-HMAC-Timestamp', 0))
+                if not hmac_timestamp:
+                        return 'HMAC timestamp missing from request', 403
+
+                if not verify_hmac(request.path, hmac_timestamp, hmac_header):
+                        return 'HMAC validation failed', 403
+
+                build_hash = request.args.get('build_hash', '')
+                if not build_hash:
+                        return 'Build hash argument required', 400
+
+        elif build_url.startswith('file://') and request.remote_addr == HOST_IP:
+                remote_build = False
+        else:
+                return 'Invalid build URL', 400
 
         # Validate the branch name. Disallow semi-colon and then use git's
         # method for testing for valid names.
@@ -75,41 +82,52 @@ def zeek():
         # Normalize the branch name to remove any non-alphanumeric characters so it's
         # safe to use as part of a path name. This is way overkill, but it's safer.
         normalized_branch = ''.join(x for x in branch if x.isalnum())
-        normalized_branch += '-{:d}-{:d}'.format(int(hmac_timestamp), int(time.time()))
+        if remote_build:
+                normalized_branch += '-{:d}-{:d}'.format(int(hmac_timestamp), int(time.time()))
+        else:
+                normalized_branch += '-local-{:d}'.format(int(time.time()))
         base_path = os.path.dirname(os.path.abspath(__file__))
         work_path = os.path.join(base_path, normalized_branch)
 
         data_file_path = os.path.dirname(DATA_FILE)
         data_file_name = os.path.basename(DATA_FILE)
         tmpfs_path = '/mnt/data/tmpfs'
+        filename = build_url.rsplit('/', 1)[1]
 
         result = None
         try:
                 os.mkdir(work_path, mode=0o700)
 
-                filename = os.path.join(work_path, build_url.rsplit('/', 1)[1])
-                r = requests.get(build_url, allow_redirects=True)
-                if not r:
-                        raise RuntimeError('Failed to download build file')
+                if remote_build:
+                        dockerfile = 'Dockerfile.runner'
+                        file_path = os.path.join(work_path, filename)
+                        r = requests.get(build_url, allow_redirects=True)
+                        if not r:
+                                raise RuntimeError('Failed to download build file')
 
-                open(filename, 'wb').write(r.content)
-                open('{:s}.md5'.format(filename), 'w').write('{:s} {:s}'.format(build_hash, filename))
+                        open(file_path, 'wb').write(r.content)
+                        open('{:s}.md5'.format(file_path), 'w').write('{:s} {:s}'.format(build_hash, file_path))
 
-                # Validate checksum of file before untarring it. There is a module in python
-                # to do this, but I'm not going to read the whole file into memory to do it.
-                ret = subprocess.call(['md5sum', '-c', '{:s}.md5'.format(filename)],
-                                      stdout=subprocess.DEVNULL)
-                if ret:
-                        raise RuntimeError('Failed to validate checksum of file')
+                        # Validate checksum of file before untarring it. There is a module in python
+                        # to do this, but I'm not going to read the whole file into memory to do it.
+                        ret = subprocess.call(['md5sum', '-c', '{:s}.md5'.format(file_path)],
+                                              stdout=subprocess.DEVNULL)
+                        if ret:
+                                raise RuntimeError('Failed to validate checksum of file')
+                else:
+                        dockerfile = 'Dockerfile.localrunner'
+                        file_path = build_url[7:]
+                        shutil.copytree(file_path, os.path.join(work_path, filename))
 
                 # Build new docker image from the base image, tagged with the normalized branch name
                 # so that we can use/delete it more easily.
                 try:
                         docker_client.images.build(tag=normalized_branch, path=work_path, rm=True,
-                                                   dockerfile=os.path.join(base_path, 'Dockerfile.runner'),
+                                                   dockerfile=os.path.join(base_path, dockerfile),
                                                    container_limits={'cpusetcpus': '{:d},{:d}'.format(
                                                            app.config['CPU_SET'][0], app.config['CPU_SET'][1])},
-                                                   buildargs={'TMPFS_PATH': tmpfs_path})
+                                                   buildargs={'TMPFS_PATH': tmpfs_path,
+                                                              'BUILD_FILE_NAME': filename,})
 
                 except docker.errors.BuildError as be:
                         app.logger.error(be)
@@ -127,6 +145,7 @@ def zeek():
                                 remove=True, network='zeek-internal', cap_add=['SYS_NICE'],
                                 security_opt=['seccomp={:s}'.format(seccomp)],
                                 environment={
+                                        'BUILD_FILE_NAME': filename,
                                         'DATA_FILE_PATH': data_file_path,
                                         'DATA_FILE_NAME': data_file_name,
                                         'TMPFS_PATH': tmpfs_path,
@@ -163,4 +182,4 @@ def zeek():
         return result
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=12345, threaded=False, ssl_context="adhoc")
+        app.run(host=HOST_IP, port=12345, threaded=False, ssl_context="adhoc")
