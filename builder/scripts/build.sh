28523b95ff4a83f65eaf8136ecd88cb1ef832f8b
Tim Wojtulewicz <tim@corelight.com>
2021-06-21 17:08:17 -0700

    Move ci-based scripts to separate directory, add builder version

---
+++ builder/scripts/build.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+SCRIPT_PATH=/benchmark
+INSTALL_DIR=${SCRIPT_PATH}/install
+SOURCE_DIR=${SCRIPT_PATH}/zeek
+AF_PACKET_DIR=${SCRIPT_PATH}/zeek-af_packet-plugin
+
+# clone master
+echo
+echo "=== Cloning Zeek master branch ==="
+git clone --recursive https://github.com/zeek/zeek ${SOURCE_DIR} || exit
+
+# clone af_packet if it doesn't exist
+if [ ! -d ${AF_PACKET_DIR} ]; then
+    echo
+    echo "=== Cloning AF_Packet plugin ==="
+    git clone --recursive https://github.com/j-gras/zeek-af_packet-plugin ${AF_PACKET_DIR} || exit
+fi
+
+# configure master with af_packet plugin
+echo
+echo "=== Configuring build ==="
+cd ${SOURCE_DIR}
+./configure --generator=Ninja --build-type=relwithdebinfo --disable-python --disable-broker-tests --disable-zkg --disable-btest --disable-btest-pcaps --include-plugins=${AF_PACKET_DIR} --prefix=${INSTALL_DIR} || exit
+
+# build/install
+echo
+echo "=== Building and installing ==="
+cd build
+ninja install || exit
+
+if [ ${SKIP_ZEEK_DEPLOY:-0} -ne 1 ]; then
+
+    # copy zeekctl config
+    echo
+    echo "=== Copying zeekctl configuration ==="
+    cp ${SCRIPT_PATH}/configs/zeekctl/*.cfg ${INSTALL_DIR}/etc
+    cp ${SCRIPT_PATH}/configs/zeekctl/broker_metrics_port.py ${INSTALL_DIR}/lib64/zeek/python/zeekctl/plugins
+
+    # We need a couple of network interfaces to run t-rex against, but we
+    # unfortunately can't do this as part of the Dockerfile due to
+    # permissions during the build. Do it here instead.
+    echo "=== Creating network interfaces for Zeek/T-Rex ==="
+    ip link add veth0 type veth peer name veth1
+    ip addr add 192.168.1.1 dev veth0
+    ip addr add 192.168.2.1 dev veth1
+    ip link set veth0 up
+    ip link set veth1 up
+
+    # We can't run cron from systemd inside a docker container so just start
+    # it up manually
+    echo "*/5 * * * * /usr/local/zeek/bin/zeekctl cron" | crontab -
+    crond
+
+    # start up zeek
+    echo
+    echo "=== Starting zeek ==="
+    export PATH=${INSTALL_DIR}/bin:${PATH}
+    zeekctl deploy || exit
+
+fi
+
+if [ ${SKIP_TREX:-0} -ne 1 ]; then
+
+    # start up t-rex
+    #
+    # explanation of options here:
+    # --cfg: the path to the config file we copied during Docker build
+    # -f: the path to traffic configuration. sfr3.yaml is a general mix of protocols.
+    # -m: the number of times the data in the traffic configuration is multiplied.
+    #     by default srf3.yaml is about 100Mbps. setting this value higher increases
+    #     the amount of data sent.
+    # -d: the duration of the test
+    # --nc: shut down quickly at the end of the duration without letting the flows
+    #       fully expire.
+    echo
+    echo "=== Starting t-rex ==="
+    cd ${SCRIPT_PATH}/trex/v2.89
+    ./t-rex-64 --cfg ${SCRIPT_PATH}/configs/trex_cfg.yaml -f cap2/sfr3.yaml -m 3 -d 300 --nc
+
+fi
